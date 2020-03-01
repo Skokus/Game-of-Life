de328c8e2ee08bda44680683ae970712a7788175
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-01 19:22:07 +0100

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -3,12 +3,13 @@
 
 int main()
 {
-    int macierz[2][2] = {
-    {1,2},
-    {3,4},
+    int macierz[3][3] = {
+    {1,0,0},
+    {1,1,0},
+    {0,1,0},
     };
-    for(int r = 0; r < 2; r++){
-        for(int c = 0; c<2; c++)
+    for(int r = 0; r < 3; r++){
+        for(int c = 0; c<3; c++)
             printf("%d ", macierz[r][c]);
 
     printf("\n");
