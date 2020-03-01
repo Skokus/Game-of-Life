9ab9e75576022a6e604c74400ff14513ccbf4634
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-01 22:15:50 +0100

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -4,32 +4,41 @@
 void checksasiad(int macierz[3][3], int sasiad[3][3]){
     for(int r = 0; r < 3; r++){
         for(int c = 0; c < 3; c++){
-            if(r-1 >= 0 && c-1 >= 0)
-                if(macierz[r-1][c-1] == 1)
-                    sasiad[r][c]++;
-            if(r-1 >= 0 && c >= 0)
+            if(r-1 >=0){
+                if(c-1 >= 0)
+                    if(macierz[r-1][c-1] == 1)
+                        sasiad[r][c]++;
+
                 if(macierz[r-1][c] == 1)
                     sasiad[r][c]++;
-            if(r-1 >= 0 && c+1 >= 0)
-                if(macierz[r-1][c+1] == 1)
-                    sasiad[r][c]++;
-            if(r >= 0 && c-1 >= 0)
-                if(macierz[r][c-1] == 1)
-                    sasiad[r][c]++;
-            if(r >= 0 && c+1 >= 0)
-                if(macierz[r][c+1] == 1)
-                    sasiad[r][c]++;
-            if(r+1 >= 0 && c-1 >= 0)
-                if(macierz[r+1][c-1] == 1)
-                    sasiad[r][c]++;
-            if(r+1 >= 0 && c >= 0)
-                if(macierz[r+1][c] == 1)
-                    sasiad[r][c]++;
-            if(r+1 >= 0 && c+1 >= 0)
-                if(macierz[r+1][c+1] == 1)
-                    sasiad[r][c]++;
+
+                if(c+1 < 3)
+                    if(macierz[r-1][c+1] == 1)
+                        sasiad[r][c]++;
+            }
+
+                if(c-1 >= 0)
+                    if(macierz[r][c-1] == 1)
+                        sasiad[r][c]++;
+
+                if(c+1 < 3)
+                    if(macierz[r][c+1] == 1)
+                        sasiad[r][c]++;
+            if(r+1 < 3){
+                if(c-1 >= 0)
+                    if(macierz[r+1][c-1] == 1)
+                        sasiad[r][c]++;
+
+                    if(macierz[r+1][c] == 1)
+                        sasiad[r][c]++;
+
+                if(c+1 < 3)
+                    if(macierz[r+1][c+1] == 1)
+                        sasiad[r][c]++;
+            }
         }
     }
 }
 
 #endif MATRIX_H_
+
