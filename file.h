a1ee848d7a564b9889e5c4aeec79daf48f551b60
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-06 17:34:56 +0100

    Update file.h

--- file.h
+++ file.h
@@ -1,45 +1,9 @@
 #ifndef FILE_H_
 #define FILE_H_
 
+void zwymiarujmacierz(int *rows, int *colm, int *colmmax, FILE *fp);
 
-void zwymiarujmacierz(int *rows, int *colm, int *colmmax, FILE *fp){
-    int c;
-    while((c = fgetc(fp)) != EOF){
-        if (c == '\n'){
-            (*rows)++;
-            *colm = 0;
-        }
-        else if (c == '1'){
-            (*colm)++;
-            if(*colm > *colmmax)
-                *colmmax = *colm;
-        }
-        else if (c == '0'){
-            (*colm)++;
-            if(*colm > *colmmax)
-                *colmmax = *colm;
-        }
-    }
-    *colm = *colmmax;
-}
+void wczytajmacierz(int rows, int colm, int macierz[rows][colm], FILE *file);
 
-void wczytajmacierz(int rows, int colm, int macierz[rows][colm], FILE *file){
-    rows = 0;
-    colm = 0;
-    int c;
-    while((c = fgetc(file)) != EOF){
-        if (c == '\n'){
-            rows++;
-            colm = 0;
-        }
-        else if (c == '1'){
-            macierz[rows][colm] = 1;
-            colm++;
-        }
-        else if (c == '0'){
-            macierz[rows][colm] = 0;
-            colm++;
-        }
-    }
-}
 #endif FILE_H_
+
