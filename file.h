e40d8122735250c81d2657042f1837bb10db1ae1
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-06 17:38:44 +0100

    Update file.h

--- file.h
+++ file.h
@@ -1,6 +1,8 @@
 #ifndef FILE_H_
 #define FILE_H_
 
+#include <stdio.h>
+
 void zwymiarujmacierz(int *rows, int *colm, int *colmmax, FILE *fp);
 
 void wczytajmacierz(int rows, int colm, int macierz[rows][colm], FILE *file);
