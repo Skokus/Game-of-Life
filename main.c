fa8269683df7736aeb71d9a7334dc6668a30d082
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-06 14:52:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,31 +3,34 @@
 
 
 #include "matrix.h"
+#include "file.h"
 
-int main() {
 
-    int rows = 5;
-    int colm = 5;
-    int iteration = 3;
+int main(int argc, char** argv) {
+    int rows = 0;
+    int colm = 0;
+    int colmmax = 0;
+    int iteration = 5;
+
+    FILE *fp = fopen(argv[1], "r");
+    zwymiarujmacierz(&rows, &colm, &colmmax, fp);
+    fclose(fp);
 
     int sasiad  [rows][colm];
-    zerujmacierz(rows, colm, sasiad);
     int macierz [rows][colm];
 
-        for(int r = 0; r < rows; r++){
-                for(int c = 0; c < colm; c++)
-                        macierz[r][c] = 1;
-                }
+    FILE *file = fopen(argv[1], "r");
+    wczytajmacierz(rows, colm, macierz, file);
+    fclose(file);
 
 
+    printf("Rows = %d, Colm = %d\n\n", rows, colm);
     for(int i = 1; i <= iteration; i++){
         printf("%d. iteracja\n", i);
+        zerujmacierz(rows, colm, sasiad);
         wypiszmacierz(rows, colm, macierz);
         checksasiad(rows, colm, macierz, sasiad);
         zmienstan(rows, colm, macierz, sasiad);
-        zerujmacierz(rows, colm, sasiad);
-
     }
     return 0;
 }
-
