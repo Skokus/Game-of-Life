5b3b33ad27c8adc0172fc12203bf04ad36850a97
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-08 15:08:49 +0100

    Poprawione tworzenie macierzy, dodanie opcji tworzenia plików

--- main.c
+++ main.c
@@ -4,20 +4,20 @@
 
 #include "matrix.h"
 #include "file.h"
-
+#include "pbm.h"
 
 int main(int argc, char** argv) {
     int rows = 0;
     int colm = 0;
     int colmmax = 0;
-    int iteration = 5;
+    int iteration = 3;
 
     FILE *fp = fopen(argv[1], "r");
     zwymiarujmacierz(&rows, &colm, &colmmax, fp);
     fclose(fp);
 
-    int sasiad  [rows][colm];
-    int macierz [rows][colm];
+    int** sasiad  = zrobmacierz(rows, colm);
+    int** macierz = zrobmacierz(rows, colm);
 
     FILE *file = fopen(argv[1], "r");
     wczytajmacierz(rows, colm, macierz, file);
@@ -25,12 +25,17 @@ int main(int argc, char** argv) {
 
 
     printf("Rows = %d, Colm = %d\n\n", rows, colm);
-    for(int i = 1; i <= iteration; i++){
+    for(int i = 1; i < iteration; i++){
         printf("%d. iteracja\n", i);
         zerujmacierz(rows, colm, sasiad);
         wypiszmacierz(rows, colm, macierz);
+        zrobObraz(rows, colm, macierz, i);
         checksasiad(rows, colm, macierz, sasiad);
         zmienstan(rows, colm, macierz, sasiad);
     }
+    printf("%d. iteracja\n", iteration);    // pętla zapisywała iteration + 1 iterację, żeby utworzyć plik trzeba zrobić to oddzielnie.
+    wypiszmacierz(rows, colm, macierz);
+    zapiszmacierz(rows, colm, macierz, "test2");
+
     return 0;
 }
