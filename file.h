4015d292663469437a42608f2a36a2eef5d137a5
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-08 15:11:19 +0100

    Poprawki zapiszmacierz(), komentarze

--- file.h
+++ file.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 void zwymiarujmacierz(int *rows, int *colm, int *colmmax, FILE *fp);
-
+//zmienna colmmax przechowuje informacje o liczbie kolumn; zwiększa się, gdy w wierszu znajdzie więcej komórek niż poprzednio
 void wczytajmacierz(int rows, int colm, int macierz[rows][colm], FILE *file);
 
-void zapiszmacierz(int rows, int colm, int macierz[rows][colm], FILE *file, char* nazwa)
+void zapiszmacierz(int rows, int colm, int macierz[rows][colm], char* nazwa);
 
 #endif FILE_H_
