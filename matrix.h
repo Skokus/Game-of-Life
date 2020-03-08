552cb731c7061cdff662c1a1938b6b9b692d622d
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-08 15:10:35 +0100

    dodane poprawki do zróbmacierz(), dodanie komentarzy

--- matrix.h
+++ matrix.h
@@ -1,6 +1,7 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
-
+// zmienna macierz to macierz, w której zapisywane są stany poszczególnych komórek
+// zmienna sasiad to macierz, w której zapisywana jest informacja ile "1" sasiaduje z daną komórką
 void checksasiad(int rows, int colm, int macierz[rows][colm], int sasiad[rows][colm]);
 
 void zmienstan(int rows, int colm, int macierz[rows][colm], int sasiad[rows][colm]);
@@ -9,6 +10,6 @@ void zerujmacierz(int rows, int colm, int sasiad[rows][colm]);
 
 void wypiszmacierz(int rows, int colm, int macierz[rows][colm]);
 
-void zrobmacierz (int rows int colm);
+int** zrobmacierz (int rows, int colm);
 
 #endif MATRIX_H_
