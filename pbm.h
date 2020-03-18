f2c0b94415b214773c65f7fd2e46a728789b5831
Skokus <61596391+Skokus@users.noreply.github.com>
2020-03-18 17:58:03 +0100

    Update pbm.h

--- pbm.h
+++ pbm.h
@@ -4,4 +4,4 @@
 void zrobObraz(int rows, int colm, int macierz[rows][colm], int numer);
 // zmienna numer jest to numer aktualnej iteracji, bedzie on tez nazwa obrazu w ktorym iteracja jest zapisana
 
-#endif PBM_H
+#endif
